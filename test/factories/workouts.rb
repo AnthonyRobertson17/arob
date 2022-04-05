b6610f385e575827dc5cd89680d8e4753ae21a56
Anthony Robertson <arob@hey.com>
2022-04-04 21:11:47 -0400

    adding model helpers and more data to the workout partial

--- test/factories/workouts.rb
+++ test/factories/workouts.rb
@@ -4,4 +4,13 @@ FactoryBot.define do
   factory :workout do
     name { "WorkoutName" }
   end
+
+  trait :started do
+    started_at { 1.hour.ago }
+  end
+
+  trait :completed do
+    started
+    completed_at { 1.hour.ago }
+  end
 end
--- test/models/workout_test.rb
+++ test/models/workout_test.rb
@@ -3,7 +3,35 @@
 require "test_helper"
 
 class WorkoutTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "new workouts are not started or completed" do
+    workout = build :workout
+
+    assert_not workout.started?
+    assert_not workout.completed?
+  end
+
+  test "started? is true when started_at is set" do
+    workout = build :workout, :started
+
+    assert_predicate workout, :started?
+  end
+
+  test "active? is true when started but not complete" do
+    workout = build :workout, :started
+
+    assert_predicate workout, :active?
+  end
+
+  test "active? is false when completed_at is set" do
+    workout = build :workout, :completed
+
+    assert_predicate workout, :started?
+    assert_not workout.active?
+  end
+
+  test "completed is true when completed_at is set" do
+    workout = build :workout, :completed
+
+    assert_predicate workout, :completed?
+  end
 end
