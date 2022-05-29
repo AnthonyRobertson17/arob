3eb3c354609d1fe79f5023e08d0a9f0bac719a69
Anthony Robertson <arob@hey.com>
2022-05-28 23:55:01 -0400

    adding application controller tests

---
+++ test/controllers/application_controller_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestController < ApplicationController
+  def index
+    render plain: "OK"
+  end
+end
+
+class HomeControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    Rails.application.routes.draw do
+      devise_for :users
+      get "/test", to: "test#index"
+    end
+  end
+
+  test "does nothing when authenticated" do
+    user = create :user
+    sign_in user
+
+    get "/test"
+    assert_response :success
+  end
+
+  test "redirects to login when not authenticated" do
+    get "/test"
+
+    assert_redirected_to new_user_session_url
+    assert_equal "You need to sign in or sign up before continuing.", flash[:alert]
+  end
+end
