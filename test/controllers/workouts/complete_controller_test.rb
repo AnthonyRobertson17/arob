792f4aca5f4e6aff7f9239f071672fd610f7b6d1
Anthony Robertson <arob@hey.com>
2022-06-03 22:06:16 -0400

    adding controller to complete workouts

---
+++ test/controllers/workouts/complete_controller_test.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module Workouts
+  class CompleteControllerTest < ActionDispatch::IntegrationTest
+    setup do
+      user = create :user
+      sign_in user
+    end
+
+    test "update redirects to the workout show page" do
+      workout = create :workout, :started
+
+      patch complete_workout_url(workout)
+      assert_redirected_to workout_url(workout)
+      assert_equal "Workout was successfully completed.", flash[:notice]
+
+      assert_predicate workout.reload, :completed?
+    end
+
+    test "updating an already completed workout should set alert flash" do
+      workout = create :workout, :completed
+
+      patch complete_workout_url(workout)
+
+      assert_redirected_to workout_url(workout)
+      assert_equal "Workout has already been completed.", flash[:alert]
+    end
+
+    test "updating a not started workout should set alert flash" do
+      workout = create :workout
+
+      patch complete_workout_url(workout)
+
+      assert_redirected_to workout_url(workout)
+      assert_equal "Workout has not been started.", flash[:alert]
+    end
+  end
+end
