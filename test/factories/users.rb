e08405bcf25eb7f2cb6177ad3527a2861759d46e
Anthony Robertson <arob@hey.com>
2022-06-07 23:18:18 -0400

    adding EST timezone to user factory

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "jim.halpert#{n}@dundermifflin.com" }
     password { "password12345" }
+    time_zone { "Eastern Time (US & Canada)" }
   end
 end
