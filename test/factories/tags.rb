6addcae0cb8fdb42a55d88edc9a406bcc61130c8
Anthony Robertson <arob@hey.com>
2022-06-18 22:10:03 -0400

    updating workout category model and factory to tag

---
+++ test/factories/tags.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal:true
+
+FactoryBot.define do
+  factory :tag do
+    user
+    sequence(:name) { |n| "tag-#{n}" }
+    type { "foo" }
+  end
+end
--- test/factories/workout_categories.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal:true
-
-FactoryBot.define do
-  factory :workout_category do
-    user
-    sequence(:name) { |n| "category-#{n}" }
-  end
-end
