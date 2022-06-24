c0b0d3a5d59391d1802e71f48ba423533168e0c3
Anthony Robertson <arob@hey.com>
2022-06-23 22:34:11 -0400

    fixing workout_tag_assignment factory

--- test/factories/workout_tag_assignments.rb
+++ test/factories/workout_tag_assignments.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal:true
 
 FactoryBot.define do
-  factory :workout_tag_assignment
+  factory :workout_tag_assignment do
+    transient do
+      user { build(:user) }
+    end
+
+    workout { build(:workout, user:) }
+    tag { build(:workout_tag, user:) }
+  end
 end
