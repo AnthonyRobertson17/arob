d2a38cdaa1e8cfc412aee04260312dbfdeaf2900
Anthony Robertson <arob@hey.com>
2022-07-03 23:38:30 -0400

    build the user to exercise model relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :rememberable, :validatable
 
   has_many :workouts, dependent: :destroy
+  has_many :exercises, through: :workouts
 end
