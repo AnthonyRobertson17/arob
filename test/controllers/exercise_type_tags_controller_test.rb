86c22eb93f7996e6a54ffe5fd2c73fa1a579ac70
Anthony Robertson <arob@hey.com>
2022-07-07 18:06:38 -0400

    updating parenthesis style

--- test/controllers/exercise_type_tags_controller_test.rb
+++ test/controllers/exercise_type_tags_controller_test.rb
@@ -4,26 +4,26 @@ require "test_helper"
 
 class ExerciseTypeTagsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @user = create :user
-    sign_in @user
+    @user = create(:user)
+    sign_in(@user)
   end
 
   test "get index" do
-    create :exercise_type_tag, user: @user, name: "testing exercise_type tag"
+    create(:exercise_type_tag, user: @user, name: "testing exercise_type tag")
 
-    get exercise_type_tags_url
-    assert_response :success
+    get(exercise_type_tags_url)
+    assert_response(:success)
 
-    assert_select "h5", { text: /testing exercise_type tag/, count: 1 }
+    assert_select("h5", { text: /testing exercise_type tag/, count: 1 })
   end
 
   test "get index doesn't show exercise_type_tags which don't belong to the current user" do
-    create :exercise_type_tag, name: "should not be able to see this"
+    create(:exercise_type_tag, name: "should not be able to see this")
 
-    get exercise_type_tags_url
-    assert_response :success
+    get(exercise_type_tags_url)
+    assert_response(:success)
 
-    assert_select "h5", { text: /should not be able to see this/, count: 0 }
+    assert_select("h5", { text: /should not be able to see this/, count: 0 })
   end
 
   test "get index shows workout_tags in case insensitive alphabetical order" do
@@ -42,117 +42,119 @@ class ExerciseTypeTagsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "get new" do
-    get new_exercise_type_tag_url
-    assert_response :success
+    get(new_exercise_type_tag_url)
+    assert_response(:success)
   end
 
   test "create exercise_type_tag creates the record" do
     assert_difference("ExerciseTypeTag.count") do
-      post exercise_type_tags_url, params: { exercise_type_tag: { name: "Random Tag" } }
+      post(exercise_type_tags_url, params: { exercise_type_tag: { name: "Random Tag" } })
     end
   end
 
   test "create exercise_type_tag with html format redirects to show" do
-    post exercise_type_tags_url, params: { exercise_type_tag: { name: "Random Tag" } }
+    post(exercise_type_tags_url, params: { exercise_type_tag: { name: "Random Tag" } })
 
-    assert_redirected_to exercise_type_tag_url(ExerciseTypeTag.last)
+    assert_redirected_to(exercise_type_tag_url(ExerciseTypeTag.last))
   end
 
   test "create exercise_type_tag with turbo_stream format responds with OK" do
-    post exercise_type_tags_url(format: :turbo_stream), params: { exercise_type_tag: { name: "Random Tag" } }
+    post(exercise_type_tags_url(format: :turbo_stream), params: { exercise_type_tag: { name: "Random Tag" } })
 
-    assert_response :ok
+    assert_response(:ok)
   end
 
   test "create exercise_type_tag links to the current_user" do
-    post exercise_type_tags_url, params: { exercise_type_tag: { name: "Random Tag" } }
+    post(exercise_type_tags_url, params: { exercise_type_tag: { name: "Random Tag" } })
 
     new_exercise_type_tag = ExerciseTypeTag.for_user(@user).last
-    assert_equal "Random Tag", new_exercise_type_tag.name
+    assert_equal("Random Tag", new_exercise_type_tag.name)
   end
 
   test "show exercise_type_tag" do
-    exercise_type_tag = create :exercise_type_tag, user: @user
+    exercise_type_tag = create(:exercise_type_tag, user: @user)
 
-    get exercise_type_tag_url(exercise_type_tag)
-    assert_response :success
+    get(exercise_type_tag_url(exercise_type_tag))
+    assert_response(:success)
   end
 
   test "show exercise_type_tag raises not found if exercise_type_tag belongs to another user" do
-    exercise_type_tag = create :exercise_type_tag
+    exercise_type_tag = create(:exercise_type_tag)
 
     assert_raises(ActiveRecord::RecordNotFound) do
-      get exercise_type_tag_url(exercise_type_tag)
+      get(exercise_type_tag_url(exercise_type_tag))
     end
   end
 
   test "get edit" do
-    exercise_type_tag = create :exercise_type_tag, user: @user
+    exercise_type_tag = create(:exercise_type_tag, user: @user)
 
-    get edit_exercise_type_tag_url(exercise_type_tag)
-    assert_response :success
+    get(edit_exercise_type_tag_url(exercise_type_tag))
+    assert_response(:success)
   end
 
   test "get edit raises not found if exercise_type_tag is not for the current user" do
-    exercise_type_tag = create :exercise_type_tag
+    exercise_type_tag = create(:exercise_type_tag)
 
     assert_raises(ActiveRecord::RecordNotFound) do
-      get edit_exercise_type_tag_url(exercise_type_tag)
+      get(edit_exercise_type_tag_url(exercise_type_tag))
     end
   end
 
   test "update exercise_type_tag with html format redirects to show" do
-    exercise_type_tag = create :exercise_type_tag, user: @user
+    exercise_type_tag = create(:exercise_type_tag, user: @user)
 
-    patch exercise_type_tag_url(exercise_type_tag), params: { exercise_type_tag: { name: "New ExerciseType Name" } }
-    assert_redirected_to exercise_type_tag_url(exercise_type_tag)
+    patch(exercise_type_tag_url(exercise_type_tag), params: { exercise_type_tag: { name: "New ExerciseType Name" } })
+    assert_redirected_to(exercise_type_tag_url(exercise_type_tag))
   end
 
   test "update exercise_type_tag with turbo_stream format responss with OK" do
-    exercise_type_tag = create :exercise_type_tag, user: @user
+    exercise_type_tag = create(:exercise_type_tag, user: @user)
 
-    patch exercise_type_tag_url(exercise_type_tag, format: :turbo_stream),
-          params: { exercise_type_tag: { name: "New ExerciseType Name" } }
-    assert_response :ok
+    patch(
+      exercise_type_tag_url(exercise_type_tag, format: :turbo_stream),
+      params: { exercise_type_tag: { name: "New ExerciseType Name" } },
+    )
+    assert_response(:ok)
   end
 
   test "update exercise_type_tag raises not found if exercise_type_tag belongs to another user" do
-    exercise_type_tag = create :exercise_type_tag
+    exercise_type_tag = create(:exercise_type_tag)
 
     assert_raises(ActiveRecord::RecordNotFound) do
-      patch exercise_type_tag_url(exercise_type_tag), params: { exercise_type_tag: { name: "New ExerciseType Name" } }
+      patch(exercise_type_tag_url(exercise_type_tag), params: { exercise_type_tag: { name: "New ExerciseType Name" } })
     end
   end
 
   test "destroy exercise_type_tag destroys the record" do
-    exercise_type_tag = create :exercise_type_tag, user: @user
+    exercise_type_tag = create(:exercise_type_tag, user: @user)
 
     assert_difference("ExerciseTypeTag.count", -1) do
-      delete exercise_type_tag_url(exercise_type_tag)
+      delete(exercise_type_tag_url(exercise_type_tag))
     end
   end
 
   test "destroy exercise_type_tag with html format redirects to index" do
-    exercise_type_tag = create :exercise_type_tag, user: @user
+    exercise_type_tag = create(:exercise_type_tag, user: @user)
 
-    delete exercise_type_tag_url(exercise_type_tag)
+    delete(exercise_type_tag_url(exercise_type_tag))
 
-    assert_redirected_to exercise_type_tags_url
+    assert_redirected_to(exercise_type_tags_url)
   end
 
   test "destroy exercise_type_tag with turbo_stream format responds with OK" do
-    exercise_type_tag = create :exercise_type_tag, user: @user
+    exercise_type_tag = create(:exercise_type_tag, user: @user)
 
-    delete exercise_type_tag_url(exercise_type_tag, format: :turbo_stream)
+    delete(exercise_type_tag_url(exercise_type_tag, format: :turbo_stream))
 
-    assert_response :ok
+    assert_response(:ok)
   end
 
   test "destroy exercise_type_tag raises not found if exercise_type_tag belongs to another user" do
-    exercise_type_tag = create :exercise_type_tag
+    exercise_type_tag = create(:exercise_type_tag)
 
     assert_raises(ActiveRecord::RecordNotFound) do
-      delete exercise_type_tag_url(exercise_type_tag)
+      delete(exercise_type_tag_url(exercise_type_tag))
     end
   end
 end
--- test/controllers/workout_tags_controller_test.rb
+++ test/controllers/workout_tags_controller_test.rb
@@ -4,26 +4,26 @@ require "test_helper"
 
 class WorkoutTagsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @user = create :user
-    sign_in @user
+    @user = create(:user)
+    sign_in(@user)
   end
 
   test "get index" do
-    create :workout_tag, user: @user, name: "testing workout tag"
+    create(:workout_tag, user: @user, name: "testing workout tag")
 
-    get workout_tags_url
-    assert_response :success
+    get(workout_tags_url)
+    assert_response(:success)
 
-    assert_select "h5", { text: /testing workout tag/, count: 1 }
+    assert_select("h5", { text: /testing workout tag/, count: 1 })
   end
 
   test "get index doesn't show workout_tags which don't belong to the current user" do
-    create :workout_tag, name: "should not be able to see this"
+    create(:workout_tag, name: "should not be able to see this")
 
-    get workout_tags_url
-    assert_response :success
+    get(workout_tags_url)
+    assert_response(:success)
 
-    assert_select "h5", { text: /should not be able to see this/, count: 0 }
+    assert_select("h5", { text: /should not be able to see this/, count: 0 })
   end
 
   test "get index shows workout_tags in case insensitive alphabetical order" do
@@ -42,116 +42,116 @@ class WorkoutTagsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "get new" do
-    get new_workout_tag_url
-    assert_response :success
+    get(new_workout_tag_url)
+    assert_response(:success)
   end
 
   test "create workout_tag creates a new record" do
     assert_difference("WorkoutTag.count") do
-      post workout_tags_url, params: { workout_tag: { name: "Random Tag" } }
+      post(workout_tags_url, params: { workout_tag: { name: "Random Tag" } })
     end
   end
 
   test "create workout_tag links new record to the current_user" do
-    post workout_tags_url, params: { workout_tag: { name: "Random Tag" } }
+    post(workout_tags_url, params: { workout_tag: { name: "Random Tag" } })
 
     new_workout_tag = WorkoutTag.for_user(@user).last
-    assert_equal "Random Tag", new_workout_tag.name
+    assert_equal("Random Tag", new_workout_tag.name)
   end
 
   test "create workout_tag with html format redirects to correct workout_tag" do
-    post workout_tags_url, params: { workout_tag: { name: "Random Tag" } }
+    post(workout_tags_url, params: { workout_tag: { name: "Random Tag" } })
 
-    assert_redirected_to workout_tag_url(WorkoutTag.last)
+    assert_redirected_to(workout_tag_url(WorkoutTag.last))
   end
 
   test "create workout_tag with turbo_stream format response with OK" do
-    post workout_tags_url(format: :turbo_stream), params: { workout_tag: { name: "Random Tag" } }
+    post(workout_tags_url(format: :turbo_stream), params: { workout_tag: { name: "Random Tag" } })
 
-    assert_response :ok
+    assert_response(:ok)
   end
 
   test "show workout_tag" do
-    workout_tag = create :workout_tag, user: @user
+    workout_tag = create(:workout_tag, user: @user)
 
-    get workout_tag_url(workout_tag)
-    assert_response :success
+    get(workout_tag_url(workout_tag))
+    assert_response(:success)
   end
 
   test "show workout_tag raises not found if workout_tag belongs to another user" do
-    workout_tag = create :workout_tag
+    workout_tag = create(:workout_tag)
 
     assert_raises(ActiveRecord::RecordNotFound) do
-      get workout_tag_url(workout_tag)
+      get(workout_tag_url(workout_tag))
     end
   end
 
   test "get edit" do
-    workout_tag = create :workout_tag, user: @user
+    workout_tag = create(:workout_tag, user: @user)
 
-    get edit_workout_tag_url(workout_tag)
-    assert_response :success
+    get(edit_workout_tag_url(workout_tag))
+    assert_response(:success)
   end
 
   test "get edit raises not found if workout_tag is not for the current user" do
-    workout_tag = create :workout_tag
+    workout_tag = create(:workout_tag)
 
     assert_raises(ActiveRecord::RecordNotFound) do
-      get edit_workout_tag_url(workout_tag)
+      get(edit_workout_tag_url(workout_tag))
     end
   end
 
   test "update workout_tag with html format redirects to show" do
-    workout_tag = create :workout_tag, user: @user
+    workout_tag = create(:workout_tag, user: @user)
 
-    patch workout_tag_url(workout_tag), params: { workout_tag: { name: "New Workout Name" } }
-    assert_redirected_to workout_tag_url(workout_tag)
+    patch(workout_tag_url(workout_tag), params: { workout_tag: { name: "New Workout Name" } })
+    assert_redirected_to(workout_tag_url(workout_tag))
   end
 
   test "update workout_tag with turbo_stream format responds with OK" do
-    workout_tag = create :workout_tag, user: @user
+    workout_tag = create(:workout_tag, user: @user)
 
-    patch workout_tag_url(workout_tag, format: :turbo_stream), params: { workout_tag: { name: "New Workout Name" } }
-    assert_response :ok
+    patch(workout_tag_url(workout_tag, format: :turbo_stream), params: { workout_tag: { name: "New Workout Name" } })
+    assert_response(:ok)
   end
 
   test "update workout_tag raises not found if workout_tag belongs to another user" do
-    workout_tag = create :workout_tag
+    workout_tag = create(:workout_tag)
 
     assert_raises(ActiveRecord::RecordNotFound) do
-      patch workout_tag_url(workout_tag), params: { workout_tag: { name: "New Workout Name" } }
+      patch(workout_tag_url(workout_tag), params: { workout_tag: { name: "New Workout Name" } })
     end
   end
 
   test "destroy workout_tag destroys the record" do
-    workout_tag = create :workout_tag, user: @user
+    workout_tag = create(:workout_tag, user: @user)
 
     assert_difference("WorkoutTag.count", -1) do
-      delete workout_tag_url(workout_tag)
+      delete(workout_tag_url(workout_tag))
     end
   end
 
   test "destroy workout_tag with html format redirects to workout tag index" do
-    workout_tag = create :workout_tag, user: @user
+    workout_tag = create(:workout_tag, user: @user)
 
-    delete workout_tag_url(workout_tag)
+    delete(workout_tag_url(workout_tag))
 
-    assert_redirected_to workout_tags_url
+    assert_redirected_to(workout_tags_url)
   end
 
   test "destroy workout_tag with tubro_stream format response with OK" do
-    workout_tag = create :workout_tag, user: @user
+    workout_tag = create(:workout_tag, user: @user)
 
-    delete workout_tag_url(workout_tag, format: :turbo_stream)
+    delete(workout_tag_url(workout_tag, format: :turbo_stream))
 
-    assert_response :ok
+    assert_response(:ok)
   end
 
   test "destroy workout_tag raises not found if workout_tag belongs to another user" do
-    workout_tag = create :workout_tag
+    workout_tag = create(:workout_tag)
 
     assert_raises(ActiveRecord::RecordNotFound) do
-      delete workout_tag_url(workout_tag)
+      delete(workout_tag_url(workout_tag))
     end
   end
 end
