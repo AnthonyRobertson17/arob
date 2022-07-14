87b97a1c997f01c1cbe70739d5c5763bbe024e51
Anthony Robertson <arob@hey.com>
2022-07-14 19:23:52 -0400

    adding position with 0 as default to the exercises table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_13_022250) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_14_030556) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_13_022250) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "note"
+    t.integer "position", default: 0, null: false
     t.index ["exercise_type_id"], name: "index_exercises_on_exercise_type_id"
     t.index ["workout_id"], name: "index_exercises_on_workout_id"
   end
