ad5f321edf29054705551a3685e90aa761fbf3b3
Anthony Robertson <arob@hey.com>
2022-07-14 21:12:04 -0400

    ensure that position is set before validation

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -5,11 +5,13 @@ class Exercise < ApplicationRecord
   belongs_to :exercise_type
   has_many :exercise_sets, dependent: :destroy
 
+  before_validation :set_position
+
   validates :position, uniqueness: { scope: :workout }
 
-  before_create :set_position
-
   def set_position
+    return unless new_record?
+
     last_position = workout.exercises.order(position: :desc).first&.position
     self.position = if last_position.present?
                       last_position + 1
