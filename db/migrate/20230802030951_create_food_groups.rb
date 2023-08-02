e993b7f82560a60bb329eb39ae7e8bab724f7dcc
Anthony Robertson <arob@hey.com>
2023-08-01 23:13:38 -0400

    create food_groups table

---
+++ db/migrate/20230802030951_create_food_groups.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreateFoodGroups < ActiveRecord::Migration[7.0]
+  def change
+    create_table(:food_groups) do |t|
+      t.string(:name, null: false)
+      t.string(:emoji, limit: 1, null: false)
+
+      t.belongs_to(:user)
+
+      t.timestamps
+    end
+  end
+end
